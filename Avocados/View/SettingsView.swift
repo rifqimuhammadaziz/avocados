cd2ae6ac851f69593e9b7b7c712c298f5295ec86
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-07-29 10:45:56 +0700

    create settings view

--- Avocados/View/SettingsView.swift
+++ Avocados/View/SettingsView.swift
@@ -8,8 +8,86 @@
 import SwiftUI
 
 struct SettingsView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var enableNotification: Bool = true
+    @State private var backgroundRefresh: Bool = false
+    
     var body: some View {
-        Text("Settings View")
+        VStack(alignment: .center, spacing: 0) {
+            // MARK: - HEADER
+            VStack(alignment: .center, spacing: 5) {
+                Image("avocado")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.top)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .shadow(color: Color("ColorBlackTransparentLight"), radius: 8, x: 0, y: 4)
+                
+                Text("Avocados".uppercased())
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                .foregroundColor(Color("ColorGreenAdaptive"))
+            }
+            .padding()
+            
+            Form {
+                // MARK: - SECTION #1
+                Section(header: Text("General")) {
+                    Toggle(isOn: $enableNotification) {
+                        Text("Enable Notification")
+                    }
+                    
+                    Toggle(isOn: $backgroundRefresh) {
+                        Text("Background Refresh")
+                    }
+                }
+                
+                // MARK: - SECTION #2
+                Section(header: Text("Application")) {
+                    if enableNotification {
+                        HStack {
+                            Text("Product").foregroundColor(Color.gray)
+                            Spacer()
+                            Text("Avocado Recipes")
+                        }
+                        HStack {
+                            Text("Compatibility").foregroundColor(Color.gray)
+                            Spacer()
+                            Text("iPhone & iPad")
+                        }
+                        HStack {
+                            Text("Developer").foregroundColor(Color.gray)
+                            Spacer()
+                            Text("Rifqi Muhammad Aziz")
+                        }
+                        HStack {
+                            Text("Designer").foregroundColor(Color.gray)
+                            Spacer()
+                            Text("Xenosty")
+                        }
+                        HStack {
+                            Text("Website").foregroundColor(Color.gray)
+                            Spacer()
+                            Text("Xenostech")
+                        }
+                        HStack {
+                            Text("Version").foregroundColor(Color.gray)
+                            Spacer()
+                            Text("1.0.0")
+                        }
+                    } else {
+                        HStack {
+                            Text("Personal Message").foregroundColor(Color.gray)
+                            Spacer()
+                            Text("github.com/rifqimuhammadaziz")
+                        }
+                    }
+                    
+                }
+            }
+        }
+        .frame(maxWidth: 640)
     }
 }
 
